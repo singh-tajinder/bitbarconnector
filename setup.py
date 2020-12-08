b8a00a8954b59ba5c1cef459626dcbe1d17bf30d
Tajinder Singh <68238398+singh-tajinder@users.noreply.github.com>
2020-12-08 15:13:32 +0100

    updated the release number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Tajinder Singh',                   # Type in your name
   author_email = 'tajinder.singh1985@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/singh-tajinder/bitbarconnector',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/singh-tajinder/bitbarconnector/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/singh-tajinder/bitbarconnector/archive/v_01.01.tar.gz',    # I explain this later on
   keywords = ['bitbar', 'andriod testing', 'bitbar apis'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'testdroid',

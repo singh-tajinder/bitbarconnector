1daebf5d2728dcf2137dc134ee96a5b9f8106a71
Tajinder Singh <68238398+singh-tajinder@users.noreply.github.com>
2020-12-08 15:31:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'bitbarconnector',         # How you named your package folder (MyLib)
   packages = ['bitbarconnector'],   # Chose the same as "name"
-  version = '0.1.2',      # Start with a small number and increase it with every change you make
+  version = '0.1.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This library helps you connect with bitbar apis and run andriod instrument tests',   # Give a short description about your library
   author = 'Tajinder Singh',                   # Type in your name
   author_email = 'tajinder.singh1985@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/singh-tajinder/bitbarconnector',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/singh-tajinder/bitbarconnector/archive/v_01.02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/singh-tajinder/bitbarconnector/archive/v_01.03.tar.gz',    # I explain this later on
   keywords = ['bitbar', 'andriod testing', 'bitbar apis'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'testdroid',
